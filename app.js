7fa12ccefef3fc98c573a918552727506716b962
Paulo Fortes <paulorobertoroco@gmail.com>
2024-09-30 16:07:52 -0300

    se creó página comunidad y archivo .js

---
+++ app.js
@@ -0,0 +1,9 @@
+function showMenuHamburguesa() {
+    const menuHamburguesa = document.querySelector('.menuHamburguesa')
+    menuHamburguesa.style.display = 'flex'
+}
+
+function hideMenuHamburguesa() {
+    const menuHamburguesa = document.querySelector('.menuHamburguesa')
+    menuHamburguesa.style.display = 'none'
+}
